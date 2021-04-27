3a822ce11c048555e5463cbaafc1065f29b59cf2
John Canessa <8019504+JohnCanessa@users.noreply.github.com>
2021-04-27 14:52:31 -0500

    Add files via upload
    Added a new implementation which runs faster than the two previous implementations.

--- Solution.java
+++ Solution.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 
@@ -7,6 +8,7 @@ import java.util.Scanner;
  */
 public class Solution {
 
+    
     /**
      * Flags if an integer is positive or negative.
      */
@@ -17,9 +19,11 @@ public class Solution {
 
     /**
      * Implement atoi which converts a string to an integer.
-     * 4 ms. faster than 29.18%
+     * 
+     * Runtime: 4 ms, faster than 18.46% of Java online submissions.
+     * Memory Usage: 39 MB, less than 38.01% of Java online submissions.
      */
-    static int myAtoi(String str) {
+    static int myAtoi1(String str) {
         
         // **** list of digits ****
         String digits = "0123456789";
@@ -27,27 +31,27 @@ public class Solution {
         // **** value to be returned ****
         int result = 0;
 
-        // **** trim the string ****
+        // **** remove leading and trailing spaces ****
         str = str.trim();
 
-        // **** check initial string ****
+        // **** sanity check ****
         if (str.equals("") || str.equals("+") | str.equals("-"))
            return 0;
 
-        // **** for starters ****
+        // **** positive sign****
         SIGN sign = SIGN.POS;
 
         // **** check for leading sign ****
         if ((str.charAt(0) == '-') || (str.charAt(0) == '+')) {
 
-            // **** ****
+            // **** negative value ****
             if (str.charAt(0) == '-')
                 sign = SIGN.NEG;
     
             // **** skip leading sign ****
             str = str.substring(1);
 
-             // **** check if we have an empty string ****
+            // **** check if we have an empty string ****
             if (str.equals(""))
                 return 0;
         }
@@ -57,7 +61,7 @@ public class Solution {
 
         // **** for ease of use ****
         str = words[0];
-
+        
         // **** check if we have an empty string ****
         if (str.equals(""))
             return 0;
@@ -100,9 +104,12 @@ public class Solution {
 
     /**
      * Implement atoi which converts a string to an integer.
-     * 3 ms faster than 44.45%
+     * 
+     * 3 ms, faster than 25.31% of Java online submissions.
+     * Memory Usage: 38.6 MB, less than 96.38% of Java online submissions.
+     * 
      */
-    static int atoi(String str) {
+    static int myAtoi0(String str) {
 
         // **** list of digits ****
         String digits = "0123456789";
@@ -140,10 +147,6 @@ public class Solution {
                 break;
             }
 
-            // **** update the result (does NOT throw exception) ****
-            // result *= 10;
-            // result += ch - '0';
-
             // **** update the result (throws exception) ****
             try {
                 result = Math.multiplyExact(result, 10);
@@ -159,6 +162,69 @@ public class Solution {
         // **** return positive or negative result ****
         return sign == SIGN.POS ? result : -result;
     }
+
+
+
+
+
+
+
+    /**
+     * Implement atoi which converts a string to an integer.
+     * 
+     * Runtime: 2 ms, faster than 54.72% of Java online submissions.
+     * Memory Usage: 39.2 MB, less than 20.93% of Java online submissions.
+     */
+    static int myAtoi(String str) {
+
+        // **** sanity checks(s) ****
+        if (str.length() == 0)
+            return 0;
+
+        // **** initialization ****
+        int i       = 0;
+        int sign    = 1;
+        int result  = 0;
+
+        // **** discard leading white space(s) ****
+        while (i < str.length() && str.charAt(i) == ' ')
+            i++;
+
+        // **** check for blank string ****
+        if (str.length() == 0)
+            return 0;
+            
+        // **** assign sign ****
+        if (i < str.length() && (str.charAt(i) == '+' || str.charAt(i) == '-')){
+            if (str.charAt(i) == '-') {
+                sign = -1;
+                i++;
+            } else if (str.charAt(i) == '+'){
+                sign = 1;
+                i++;
+            }
+        }   
+
+        // **** process digits (compute result) ****
+        while (i < str.length() && str.charAt(i) >= '0' && str.charAt(i) <= '9') {
+
+            // **** check if we overflowed ****
+            if (result > Integer.MAX_VALUE / 10 || 
+                (result == Integer.MAX_VALUE / 10 && str.charAt(i) - '0' > Integer.MAX_VALUE % 10)) {
+                return (sign == 1) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
+
+            // **** multiply by 10 and add current digit (update result) ****
+            result = result * 10 + (str.charAt(i++) - '0');
+        }
+
+        // **** return signed result ****
+        return sign * result;
+    }
+
+
+
+
 
 
     /**
@@ -179,10 +245,13 @@ public class Solution {
         System.out.println("main <<< str ==>" + str + "<==");
 
         // **** convert string to integer (if possible) and display value ****
-        System.out.println("main <<< myAtoi: " + myAtoi(str));
+        System.out.println("main <<< myAtoi0: " + myAtoi0(str));
 
         // **** convert string to integer (if possible) and display value ****
-        System.out.println("main <<<   atoi: " + atoi(str));
+        System.out.println("main <<< myAtoi1: " + myAtoi1(str));
+
+        // **** convert string to integer (if possible) and display value ****
+        System.out.println("main <<< myAtoi: " + myAtoi(str));
     }
 
  }
